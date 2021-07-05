4179191dd98d5c65ebdbffd8b2dba648d0997ecb
narwarias <76558549+narwarias@users.noreply.github.com>
2021-07-05 16:00:54 +0530

    Add files via upload

---
+++ project 43/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    text("A T E D A S B A",100,300)
+    fill("light blue")
+    text("Hint:stores all information !!", 100,320)
+
+    fill("white")
+    text("C U T N I F O N",780,200)
+    fill("light blue")
+    text("Hint:performs a particular task !!", 780,220)
+
+
+}
